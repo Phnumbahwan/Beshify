b7c9cec10deb130f517bc0104cd05128a3149139
Free Wifi <quickerz59@gmail.com>
2023-07-10 11:45:49 +0800

    Fixed mobile responsiveness and web title

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
 
   return (
     <div class="flex justify-center mt-[100px]">
-      <div class='flex flex-col'>
+      <div class='flex flex-col w-[600px] mx-[20px]'>
         <div class='flex justify-center'>
           <div class='cursor-pointer' style={{ fontFamily: 'Dancing Script', fontSize: '60px' }}>
             <p><span className='text-[#FFCC33] hover:text-[#FFBF00]'>BESHIFY</span><span class='font-sans text-base font-medium'>By: Gil</span></p>
           </div>
         </div>
-        <div class='flex w-[600px]'>
+        <div class='flex justify-center w-full'>
           <input value={text} onChange={(e) => setText(e.target.value)} onKeyDown={handlePressedEnter} type="text" id="text-input" class="bg-yellow-50 border ring-yellow-100 border-yellow-500 text-yellow-900 placeholder-yellow-700 text-sm rounded-lg focus:ring-2 focus:border-1 block w-full p-2.5 focus:outline-none" placeholder="Write something here beshy..." required />
           <div class={`w-7 m-auto mx-2 hover:animate-bounce transition-opacity duration-500 ${isVisible ? 'opacity-100 delay-500' : 'opacity-0'}`}>
             <img class='w-full cursor-pointer' src={send} alt="Custom Icon" onClick={handleClick} />
